8aa664804869a9857bf27572e6600a1ca4462ed4
Pexo <contact@pexogamedev.pl>
2018-02-26 18:57:03 +0100

    Fixed naming

---
+++ oldIndex.php
@@ -0,0 +1,169 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Pexo GameDev">
+    <meta name="author" content="Pexo">
+    <link rel="icon" href="bootstrap/graphics/pexo_logo.png">
+    <title>Pexo GameDev</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="theme.css" rel="stylesheet">
+</head>
+<body>
+    <div class="main">
+        <div class="welcome">
+            <h1>Hi! Welcome on my page!</h1>
+            <p>I'm Pexo, young, aspiring game developer. 
+            <br />Here on this website you will be able to take a look at everything I have done so far and also find some info about me.</p>
+        </div>
+        <div class="full">
+            <div class="header">
+                <h1>My Projects</h1>
+            </div>
+            <div class="list-group">
+                <table>
+                    <tr>
+                        <td>
+                            <a href="http://neonnebula.pexogamedev.pl" class="list-group-item">
+                                <h4 class="list-group-item-heading">Neon Nebula</h4>
+                                <p class="list-group-item-text">My very first game ever... finished. Typical arcade-like space shooter.</p>
+                                <img class="item-list-header" src="bootstrap/graphics/neonnebulaheader.png" />
+                            </a>
+                        </td>
+                        <td>
+                            <a href="" class="list-group-item">
+                                <h4 class="list-group-item-heading">Little Secret!</h4>
+                                <p class="list-group-item-text">It's not a title, I'm just working on something that I am not ready to reveal yet. Stay tuned ;).</p>
+                                <img class="item-list-header" src="bootstrap/graphics/unknown.png" />
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <a href="">Up to top</a>
+        </div>
+        <div class="full">
+        <div class="header">
+            <h1>About Me</h1>
+        </div>
+        <i>TL;DR; I live in Poland and want to make games professionally in the future.</i><br /><br />
+            <p style="text-align:justify;">
+
+                &#9672; I was born in the same year as first Half-Life came out, which makes me younger than 2 decades (yet). I wanted to create video games since I remember and it is still a goal of my career.
+            </p>
+            <p style="text-align:justify;">
+                &#9672; For several months, now, I was working with Unity in C# and I eventually managed to develop my <a href="http://neonnebula.pexogamedev.pl">first "fully playable" game</a>. 
+                Right now I'm working on another, small project of my own,
+                but I also have gotten involved in bigger, more professional, project in which I became a part of the team.
+            </p>
+            <p style="text-align:justify;">
+                &#9672; Programming is my main niche and in the future I aspire to be an AI specialist, althought, I've got to admit, that the thing I love most in whole game development process is designing. (Kudos to best stories and characters!)
+            </p>
+            <p style="text-align:justify;">
+                &#9672; Except for games and gamedev my hobbies are <span title="If it matters anything to you I'm enneagram's five and MBTI's INTP">psychology</span>, all branches of science (especially math and quantum physics) and self-development. 
+            </p>
+            <br />
+            <div style="text-align:center">
+                <h3>But first of all - I am a gamer</h3>
+                    <span title="Portal">I know that cake is a lie.</span><br />
+                    <span title="Magicka">I know that a secret word is a BANNANA.</span><br />
+                    <span title="Well... Half-Life">I know that Half-Life 3 is confirmed.<br /></span>
+                    <span title="Heavy Rain">I know who's Origami Killer.<br /></span>
+                    <span title="That's Konami code!">I know the <b> &#129145; &#129145; &#129147; &#129147; &#129144; &#129146; &#129144; &#129146; BA [start]</b> code.<br /></span>
+                    <span title="There is No Game">I know that "There is no game" is actually a game.<br /></span>
+                    <span title="Life is Strange">I know that Life is <s>Strange</s> so unfair.<br /></span>
+                    <span title="Overwatch">I know that heroes never die.<br /></span>
+                    <span title="Dungeones & Dragons or Pathfinder">I know how to use 20 sided dice to kill goblin in 1337 ways.<br /></span>
+                    <span title="TES: Skyrim">I was an advernturer, just like everyone else, but then I took an arrow in the knee.<br /></span>
+                    <span title="Starcraft II">I have heard already <b>a few</b> times that we require more minerals.<br /></span>
+                    <span title="The Last of Us">Yes, I cried too during giraffe cutscene.<br /></span>
+                    <span title="Dark Souls 3">Yeah… I DIED, like 31400th time already… Thanks Lord of Cinder.<br /></span>
+
+                    <span title="Well, it's just every typical gamer's struggle, now isn't it?">I have full Steam library, dozen of boxes on shelves and... nothing to play.<br /></span>
+
+                    <span title="Basically every shooter ever">Ammo 199/200? Time to reload.</span><br /><br />
+
+                    <span title="Did you find hidden titles? ;)">Eeyup. That's all me.</span>
+
+                </div>
+            <a href="">Up we go!</a>
+
+            <div class="header">
+                <h1>What Can I Do?</h1>
+            </div>
+            <div class="half skill-display">
+                <h2><kbd>Languages I Know</kbd></h2>
+                <img title="C#" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/csharp_progress_indicator.png" />
+                <img title="JavaScript" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/js_progress_indicator.png" />
+                <img title="Java" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/java_progress_indicator.png" /> 
+                <img title="PHP" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/php_progress_indicator.png" />
+                <img title="SQL" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/sql_progress_indicator.png" />
+                <img title="C++" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/cpp_progress_indicator.png" />
+            </div>
+
+            <div class="half skill-display">
+                <h2><kbd>Technologies I Worked With</kbd></h2>
+                <img title="Unity3D" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/unity_progress_indicator.png" />
+                <img title="Git Version Controll" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/git_progress_indicator.png" /> 
+                <img title="Android" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/android_progress_indicator.png" /> 
+                <img title="MySQL" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/mysql_progress_indicator.png" />
+            </div>
+            <div class="half skill-display">
+                <h2><kbd>Tools I Use</kbd></h2> 
+                <img title="Windows" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/windows_progress_indicator.png" /> 
+                <img title="Unity3D" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/unity_progress_indicator.png" /> 
+                <img title="Visual Studio 2017" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/visual_studio_progress_indicator.png" /> 
+                <img title="Photoshop CS6" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/photoshop_progress_indicator.png" /> 
+                <img title="Github" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/github_progress_indicator.png" /> 
+                <img title="Android Studio" class="progress-indicator" src="bootstrap/graphics/Progress_Indicators/android_studio_progress_indicator.png" />
+            </div>
+
+        <div class="half skill-display" style="text-align:justify;">
+            <h2><kbd>Other Things Worth Mentioning</kbd></h2>
+            &#9672; I used <a href="http://getbootstrap.com/">Bootstrap</a> library to set up this website. I also used <a href="https://wordpress.com/">Wordpress</a> with <a href="http://bestweblayout.com/products/cybergames/">Cyber Games</a> layout for Neon Nebula's website.<br /><br />
+            &#9672; I'm familiar with video editing software such as Adobe Premiere or Adobe After Efects. (<a href="https://www.youtube.com/watch?v=2QpaxPW3EA0">I did this!</a>)<br /><br />
+            &#9672; I'm still attenting high school in Poland. The profile of my class is focused on Computer Science.<br /><br />
+            &#9672; I'm active gamer, I trully do love playing games just as much as creating them. It's my lifelong passion.<br /><br />
+            &#9672; Sometimes I'm funny... No, really!<br /><br />
+        </div>
+        <div class="full">
+            <p id="disclaimer"><b>Disclaimer:</b> Please note that this scale stands for <b>[little_knowladge]-[intermediate]</b>, it does not stand for <b>[little_knowladge]-[master]</b>. Let's be honest I'm nowhere close to call myself master in any of these technologies. Yet...</p>
+            
+            <div class="header">
+                <h1>Why Do I Want To Make Games?</h1>
+            </div>
+            <p style="text-align:justify;">
+                Common question, and, in my opinion, the most important one, is "<b>WHY</b> do you want to make games?". I belive that anyone who had to answer this type of "Why" question is already aware that it's very hard to simply answer it in most of cases... But anyways,
+                so... me personally? I do want to make games because:
+            </p>
+
+            <p>
+                &#9672;       I want to create worlds into which other people will be able to dive, forget about daily problems, struggles and pains.
+                <br />&#9672; I want to create amazing, breathtaking places which others can admire in pure awe.
+                <br />&#9672; I want to create lively, true, characters with deep personalities, to which others can relate and/or create bonds with them.
+                <br />&#9672; I want to create stories, which will cause people to smile, cry, scream and dream(BIG!).
+                <br />&#9672; I want to create great moments, which will remain for long time in minds of players, and will bring grin upon their faces everytime they recall them.
+                <br />
+            </p>
+            <h3 style="font-weight:bold; text-align:center;">
+                I want to make OUR WORLD more fascinating and less formidable place. <br /> In games everyone, literally, is a hero
+                of his own story, and I want to bring more of feelings like this to people.
+            </h3>
+            <a href="">Up we go!</a>
+            <br />
+            <br />
+            <div class="header">
+                <h1>Contact</h1>
+            </div>
+            <div class="full">
+                <p>If you wish to contact me anyhow you can reach me via email: <kbd>contact[at]pexogamedev.pl</kbd><br /> I'll try to answer any mails as soon as posibble. </p>
+            </div>
+            <a href="">Up we go!</a>
+            </div>
+        </div>
+        </div>
+</body>
+</html>
