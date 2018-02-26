e688545425fd2a8e824c6eb9ff869a5cc411813e
Pexo <contact@pexogamedev.pl>
2018-02-26 18:54:27 +0100

    Changed static elements to php's files to include into main later

---
+++ head.php
@@ -0,0 +1,8 @@
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Hi I'm Pexo and THIS IS MY DOMAIN. Get it?... Anyways, here you can check out all the stuff I made. My projects, trailers and all the other cool things.">
+        <meta name="author" content="Pexo">
+        <link rel="icon" href="assets/graphics/logo.png">
+        <title>Pexo GameDev</title>
+        <link href="theme.css" rel="stylesheet">
+        <link href="animate.css" rel="stylesheet">
--- pexo.php
+++ pexo.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Hi I'm Pexo and THIS IS MY DOMAIN. Get it?... Anyways, here you can check out all the stuff I made. My projects, trailers and all the other cool things.">
-        <meta name="author" content="Pexo">
-        <link rel="icon" href="assets/graphics/logo.png">
-        <title>Pexo GameDev</title>
-        <link href="theme.css" rel="stylesheet">
-        <link href="animate.css" rel="stylesheet">
-        
+        <?php include('head.php'); ?>
     </head>
     <body>
 <div class="menu">
@@ -18,24 +10,12 @@
         <h1 class="nickname animated fadeInDown">Pexo</h1>
     </a>
     </div>
-
-    <h3>
-        <ul class="index-menu-list">
-            <a href="index.php">    <li class = "animated fadeInUp">    ♢ Whoami        </li></a>
-            <a href="projects.php"> <li class = "animated fadeInUp">    ♢ My Projects   </li></a>
-            <a href="cv.php">       <li class = "animated fadeInUp">    ♢ What Can I Do </li></a>
-            <a href="contact.php">  <li class = "animated fadeInUp">    ♢ Contact Me    </li></a>
-        </ul>
-    </h3>
+    <?php include('menu.php'); ?>
 </div>
 
 <div class="main">
     <div class="top">
-        <a href="http://github.com/pexogamedev"><img class = "github animated pulse infinite" src="assets/graphics/github.png"></a>
-        <a href="http://linkedin.com/in/owczarzpatryk/"><img class = "linkedin" src="assets/graphics/linkedindark.png"></a>
-        <a href="http://youtube.com/user/pexogames"><img class = "youtube" src="assets/graphics/yt.png"></a>
-        <a href="http://pexogamedev.pl/cv.pdf"><img class = "cv" src="assets/graphics/cv.png"></a>
-        
+        <?php include('navbarmenu.php'); ?>
     </div>
 
     <div class="content">
