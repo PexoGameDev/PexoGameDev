92034453e556bdf30bd1e94a94a9cdfe389e87a8
Pexo <contact@pexogamedev.pl>
2018-03-05 10:17:01 +0100

    .htaccess update

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <h3>
         <ul class="index-menu-list">
             <a href="http://pexogamedev.pl">    <li class = "animated fadeInUp">    ♢ Whoami        </li></a>
-            <a href="http://pexogamedev.pl/projects.php"> <li class = "animated fadeInUp">    ♢ My Projects   </li></a>
-            <a href="http://pexogamedev.pl/skills.php">       <li class = "animated fadeInUp">    ♢ What Can I Do </li></a>
-            <a href="http://pexogamedev.pl/contact.php">  <li class = "animated fadeInUp">    ♢ Contact Me    </li></a>
+            <a href="http://pexogamedev.pl/projects"> <li class = "animated fadeInUp">    ♢ My Projects   </li></a>
+            <a href="http://pexogamedev.pl/skills">       <li class = "animated fadeInUp">    ♢ What Can I Do </li></a>
+            <a href="http://pexogamedev.pl/contact">  <li class = "animated fadeInUp">    ♢ Contact Me    </li></a>
         </ul>
     </h3>
